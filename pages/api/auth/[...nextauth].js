629dc6421e489ef073dfae48febf7a2f4f0d67a8
Landon Skinner <landonjskinner@gmail.com>
2022-03-11 15:29:38 -0600

    auth folder structure and prisma schema

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  adapter: PrismaAdapter(prisma),
+  database: process.env.DATABASE_URL,
+
+})
